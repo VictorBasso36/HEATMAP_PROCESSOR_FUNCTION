bb9ddca3b9709dca8575764ec2635f6ad243ecd9
VictorBasso36 <victorbasso36@gmail.com>
2024-07-26 14:47:37 -0300

    add vercel schema file

---
+++ src/api/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import MessageResponse from '../interfaces/MessageResponse';
+
+
+const router = express.Router();
+
+router.get<{}, MessageResponse>('/', (req, res) => {
+  res.json({
+    message: 'API - HeatMap Lambda',
+  });
+});
+
+export default router;
--- src/api/maps.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { PrismaClient, Prisma } from '@prisma/client'
-const prisma = new PrismaClient()
-const router = express.Router();
-
-
-export default router;
