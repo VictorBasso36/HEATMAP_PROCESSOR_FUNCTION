358199c53a96daa9918ef286ddbb6a6e76be2aa9
VictorBasso36 <victorbasso36@gmail.com>
2024-07-29 19:43:52 -0300

    fix

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -1,17 +1,11 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../prisma/generated/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-const prisma = global.prisma || new PrismaClient({
+const prisma =  new PrismaClient({
   log: [
     { level: 'warn', emit: 'event' },
     { level: 'info', emit: 'event' },
     { level: 'error', emit: 'event' },
   ],
 });
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export default prisma;
