ee777b66948cf03afa1b0838750fb41081944545
VictorBasso36 <victorbasso36@gmail.com>
2024-07-29 18:39:46 -0300

    global client and first endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,15 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
-
 import * as middlewares from './middlewares';
 import api from './api';
 import MessageResponse from './interfaces/MessageResponse';
 
+
 require('dotenv').config();
 
 const app = express();
+
 
 app.use(morgan('dev'));
 app.use(helmet());
